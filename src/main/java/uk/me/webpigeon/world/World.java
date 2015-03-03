cfbd87f2d64474e2bd8dcf10e20cf44d31626329
CrazyEssexGuy <pwillic@essex.ac.uk>
2015-03-03 14:37:26 +0000

    Moved width and height to World

--- src/main/java/uk/me/webpigeon/world/World.java
+++ src/main/java/uk/me/webpigeon/world/World.java
@@ -6,4 +6,14 @@ import javax.swing.*;
  * Created by Piers on 03/03/2015.
  */
 public class World extends JComponent {
+
+    // width of the world
+    protected int width;
+    // height of the world
+    protected int height;
+
+    public World(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 }
