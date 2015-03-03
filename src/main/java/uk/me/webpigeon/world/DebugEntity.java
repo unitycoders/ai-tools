f9d28be7e96dfe2ec2263eced630cec183f7d1b2
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-03-03 14:56:37 +0000

    debug entity

---
+++ src/main/java/uk/me/webpigeon/world/DebugEntity.java
@@ -0,0 +1,23 @@
+package uk.me.webpigeon.world;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import uk.me.webpigeon.behavour.TreeNode;
+
+public class DebugEntity extends Entity {
+	private final Integer SIZE = 50; 
+
+	@Override
+	public void update(TreeNode behaviourRoot) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.red);
+		g2.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+	}
+
+}
