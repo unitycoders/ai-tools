7804d101415732f20fee0dc712a680bc06e19c9e
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-03-03 14:41:59 +0000

    world draw and update stuff

--- src/main/java/uk/me/webpigeon/world/World.java
+++ src/main/java/uk/me/webpigeon/world/World.java
@@ -1,9 +1,31 @@
 package uk.me.webpigeon.world;
 
+import java.awt.Graphics2D;
+import java.util.List;
+
 import javax.swing.*;
 
 /**
  * Created by Piers on 03/03/2015.
  */
 public class World extends JComponent {
+	private List<Entity> entities;
+	
+	public void addEntity(Entity entity){
+		entities.add(entity);
+	}
+	
+	public void update() {
+		
+		for (Entity entity : entities) {
+			entity.update(null);
+		}
+		
+	}
+	
+	public void draw(Graphics2D g2) {
+		for (Entity entity : entities) {
+			entity.draw(g2);
+		}
+	}
 }
