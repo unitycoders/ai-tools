6d11b2640ae85aee80ea61407209c03f2ac46976
CrazyEssexGuy <pwillic@essex.ac.uk>
2015-03-03 15:08:03 +0000

    Work on timing

--- src/main/java/uk/me/webpigeon/behavour/SequenceNode.java
+++ src/main/java/uk/me/webpigeon/behavour/SequenceNode.java
@@ -2,27 +2,26 @@ package uk.me.webpigeon.behavour;
 
 public class SequenceNode extends TreeNode {
 
-	public SequenceNode(TreeNode ... children) {
-		super(children.length);
-		for (TreeNode child : children) {
-			addChild(child);
-		}
-	}
+    public SequenceNode(TreeNode... children) {
+        super(children.length);
+        for (TreeNode child : children) {
+            addChild(child);
+        }
+    }
 
-	@Override
-	public Boolean eval() {
-		
-		Boolean result = false;
-		for (TreeNode child : getChildren()) {
-			result = child.eval();
-			if (!result) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	
+    @Override
+    public Boolean eval() {
+
+        Boolean result;
+        for (TreeNode child : getChildren()) {
+            result = child.eval();
+            if (!result) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
 }
--- src/main/java/uk/me/webpigeon/world/World.java
+++ src/main/java/uk/me/webpigeon/world/World.java
@@ -10,42 +10,73 @@ import javax.swing.*;
 /**
  * Created by Piers on 03/03/2015.
  */
-public class World extends JComponent {
-	private List<Entity> entities;
-	
+public class World extends JComponent implements Runnable {
+    private List<Entity> entities;
+
     // width of the world
     protected int width;
     // height of the world
     protected int height;
+
+    // boolean to say are we paused
+    protected boolean paused;
+    // boolean to say are we still running
+    protected boolean running;
+
+    // How long we aim to spend on each tick
+    protected int tickDelay;
+    // Time spent since last update and this update
+    protected long timeOfLastUpdate;
+    protected long previousTime;
 
     public World(int width, int height) {
         this.width = width;
         this.height = height;
         this.entities = new ArrayList<Entity>();
     }
-	
-	public void addEntity(Entity entity){
-		entities.add(entity);
-		entity.bind(this);
-	}
-	
-	public void update() {
-		
-		for (Entity entity : entities) {
-			entity.update(null);
-		}
-		
-	}
-	
-	protected void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D)g;
-		draw(g2);
-	}
-	
-	public void draw(Graphics2D g2) {
-		for (Entity entity : entities) {
-			entity.draw(g2);
-		}
-	}
-	
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+
+                Thread.sleep(tickDelay);
+                long currentTime = System.nanoTime();
+                timeOfLastUpdate = currentTime - previousTime;
+                previousTime = currentTime;
+
+                if (!paused) {
+                    update(timeOfLastUpdate);
+                    repaint();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+        entity.bind(this);
+    }
+
+    public void update(long timeOfLastUpdate) {
+
+        for (Entity entity : entities) {
+            entity.update(null);
+        }
+
+    }
+
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        draw(g2);
+    }
+
+    public void draw(Graphics2D g2) {
+        for (Entity entity : entities) {
+            entity.draw(g2);
+        }
+    }
+
 }
