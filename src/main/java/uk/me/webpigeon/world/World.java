f67c813d3f5d488e46548fe29e09ae1b9c408e22
CrazyEssexGuy <pwillic@essex.ac.uk>
2015-03-03 14:10:26 +0000

    World has size

--- src/main/java/uk/me/webpigeon/world/World.java
+++ src/main/java/uk/me/webpigeon/world/World.java
@@ -1,9 +1,12 @@
 package uk.me.webpigeon.world;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Piers on 03/03/2015.
  */
-public class World {
+public class World extends JComponent {
 
     // width in squares of the grid
     private int width;
@@ -12,7 +15,28 @@ public class World {
     // size in pixels of the grid
     private int gridSize;
 
+    public World(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.gridSize = 50;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Crazy World");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setPreferredSize(new Dimension(800, 600));
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public int getCenterOfGrid(int input){
         return (input * gridSize) + (gridSize / 2);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension((width * gridSize) + gridSize, (height * gridSize) + gridSize);
     }
 }
