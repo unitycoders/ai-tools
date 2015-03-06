20a1c6691c6f1ab5edf57a0f39baeda5438d56bb
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-03-06 20:20:14 +0000

    javadocs for the behaviour tree stuff

--- src/main/java/uk/me/webpigeon/behaviour/SequenceNode.java
+++ src/main/java/uk/me/webpigeon/behaviour/SequenceNode.java
@@ -1,5 +1,12 @@
 package uk.me.webpigeon.behaviour;
 
+/**
+ * Sequence Node
+ * 
+ * Evaluate all nodes in order from 0 to N, if all nodes return true
+ * then return true, else return false. If any node returns false then this node
+ * returns false and evaluation stops immediately.
+ */
 public class SequenceNode extends TreeNode {
 
 	public SequenceNode(TreeNode... children) {
@@ -9,6 +16,14 @@ public class SequenceNode extends TreeNode {
 		}
 	}
 
+	/**
+	 * Evaluate all child nodes in order from 0 ... size.
+	 * 
+	 * If any child fails to evaluate then evaluation of subsequent
+	 * children stop and this method returns false.
+	 * 
+	 * @return true if all nodes evaluate successfully
+	 */
 	@Override
 	public Boolean eval() {
 
